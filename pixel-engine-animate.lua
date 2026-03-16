5b40f05ea15f707472d3e52b068026719bc1fb1d
jimmisimp <jimmis.imp@gmail.com>
2026-03-16 14:07:19 -0400

    Chore: Reorganize the repo into appropriate directories.

--- pixel-engine-animate.lua
+++ pixel-engine-animate.lua
@@ -8,9 +8,9 @@ function init(plugin)
   math.randomseed(os.time())
 
   local config = load_module(plugin, "lib/pixel_engine/config.lua")
-  local support = load_module(plugin, "lib/pixel_engine/support.lua")(config)
+  local support = load_module(plugin, "lib/utils/support.lua")(config)
   local sprite_ops = load_module(plugin, "lib/pixel_engine/sprite_ops.lua")(config, support)
-  local prompt_enhance = load_module(plugin, "lib/pixel_engine/prompt_enhance.lua")(config, support)
+  local prompt_enhance = load_module(plugin, "lib/openai/prompt_enhance.lua")(config, support)
 
   command_module = load_module(plugin, "lib/pixel_engine/animate_command.lua")(config, support, sprite_ops, prompt_enhance)
   command_module.init(plugin)
